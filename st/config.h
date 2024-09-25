f19ce86953b0a4f17374e29c7d66323d89cf91bc
Eg-Nickname <djkubax@op.pl>
2024-09-25 23:49:22 +0200

    Font change

--- st/config.h
+++ st/config.h
@@ -5,8 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NF Medium:style=Medium:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+/* static char *font2[] = { "JetBrainsMonoNerdFontMono-Medium:pixelsize=12:antialias=true" }; */
+
 static int borderpx = 2;
 
 /*
